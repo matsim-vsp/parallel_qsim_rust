ad1d94721d59048a59bbe5e31df2cf02a6fe60f6
Janekdererste <janek.laudan@mailbox.org>
2022-06-03 14:36:24 +0200

    more queue experiments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod super_simple_link_automat;
+mod link_automat_with_references;
+mod link_automat_with_adjacency_list;
 
 
 fn main() {
-    super_simple_link_automat::run();
+
+    //super_simple_link_automat::run();
+    //link_automat_with_references::run();
+    link_automat_with_adjacency_list::run();
 }
