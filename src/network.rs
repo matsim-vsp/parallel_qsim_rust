f70f5aeabe61224989e0d858096a39fe14b29cc2
Janekdererste <janek.laudan@mailbox.org>
2022-06-09 12:44:53 +0200

    Start working on parsing the network

--- src/network.rs
+++ src/network.rs
@@ -1,134 +1,97 @@
-use std::collections::HashMap;
+use serde::Deserialize;
 
-use petgraph::Graph;
-use petgraph::graph::{EdgeIndex, NodeIndex};
-
-/**
-The ids are owned strings at the moment. My suspicion is that this needs to be replaced with Rc<String>
-Or an id implementation of the same type to allow passing around these ids without always cloning.
- */
+#[derive(Debug, Deserialize, PartialEq)]
 struct Node {
     id: String,
-    coord: Coord,
-}
-
-struct Link {
-    id: String,
-    freespeed: f32,
-    // ommit other parameters for later
-    from_id: String,
-    to_id: String,
-}
-
-struct Coord {
     x: f32,
     y: f32,
 }
 
+#[derive(Debug, Deserialize, PartialEq)]
+struct Link {
+    id: String,
+    from: String,
+    to: String,
+    length: f32,
+    capacity: i32,
+    freespeed: f32,
+    permlanes: i32,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Nodes {
+    #[serde(rename = "node", default)]
+    nodes: Vec<Node>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Links {
+    #[serde(rename = "link", default)]
+    links: Vec<Link>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
 struct Network {
-    graph: Graph<Node, Link>,
-    link_ids_2_index: HashMap<String, EdgeIndex>,
-    node_ids_2_index: HashMap<String, NodeIndex>,
+    name: Option<String>,
+    nodes: Nodes,
+    links: Links,
 }
 
 impl Network {
-    fn new() -> Network {
-        Network {
-            graph: Graph::new(),
-            link_ids_2_index: HashMap::new(),
-            node_ids_2_index: HashMap::new(),
-        }
+    fn nodes(&self) -> &Vec<Node> {
+        &self.nodes.nodes
     }
 
-    fn add_node(&mut self, node: Node) {
-        let node_id = node.id.clone();
-        let node_index = self.graph.add_node(node);
-        self.node_ids_2_index.insert(node_id, node_index);
-    }
-
-    fn get_node(&self, id: &str) -> &Node {
-        let node_index = self.node_ids_2_index.get(id).expect(&format!("No Node for id: {}", id));
-        self.graph.node_weight(node_index.clone()).unwrap()
-    }
-
-    fn add_link(&mut self, link: Link) {
-        let link_id = link.id.clone();
-        let from_index = self.node_ids_2_index.get(&link.from_id)
-            .expect(&format!("Node with id {} must be inserted first.", link.from_id));
-        let to_index = self.node_ids_2_index.get(&link.to_id)
-            .expect(&format!("Node with id {} must be inserted first.", link.to_id));
-        let link_index = self.graph.add_edge(from_index.clone(), to_index.clone(), link);
-        self.link_ids_2_index.insert(link_id, link_index);
-    }
-
-    fn get_link(&self, id: &str) -> &Link {
-        let link_index = self.link_ids_2_index.get(id).expect(&format!("No Link for id: {}", id));
-        self.graph.edge_weight(link_index.clone()).unwrap()
+    fn links(&self) -> &Vec<Link> {
+        &self.links.links
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::network::{Coord, Link, Network, Node};
+    use std::error::Error;
+
+    use quick_xml::de::from_str;
+
+    use crate::network::Network;
 
     #[test]
-    fn add_and_get_node() {
-        let mut network = Network::new();
-        let node = Node { id: String::from("node-id"), coord: Coord { x: 1.0, y: 1.0 } };
+    fn read_example_network() -> Result<(), Box<dyn Error>> {
+        let xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\
+                <!DOCTYPE network SYSTEM \"http://www.matsim.org/files/dtd/network_v1.dtd\">
+                <network name=\"test network\">
+                    <nodes>
+                        <node id=\"1\" x=\"-20000\" y=\"0\"/>
+                    </nodes>
+                    <links>
+                        <link id=\"23\" from=\"15\" to=\"1\" length=\"10000.00\" capacity=\"36000\" freespeed=\"27.78\" permlanes=\"1\"  />
+                    </links>
+                </network>
+            ";
 
-        network.add_node(node);
-        let result = network.get_node("node-id");
+        let result: Network = from_str(xml)?;
 
-        println!("node with id: {} and x: {} and y {}", result.id, result.coord.x, result.coord.y)
-    }
+        // test overall structure of network
+        assert_eq!("test network", result.name.as_ref().unwrap());
+        assert_eq!(1, result.nodes().len());
+        assert_eq!(1, result.links().len());
 
-    #[test]
-    fn add_and_get_link() {
-        let mut network = Network::new();
-        let from = Node { id: String::from("from-node"), coord: Coord { x: 1.0, y: 1.0 } };
-        let to = Node { id: String::from("to-node"), coord: Coord { x: 1.0, y: 1.0 } };
-        let link = Link {
-            id: String::from("link-id"),
-            freespeed: 0.0,
-            from_id: from.id.clone(),
-            to_id: to.id.clone(),
-        };
+        // test node structure
+        let node = result.nodes().first().unwrap();
+        assert_eq!("1", node.id);
+        assert_eq!(-20000., node.x);
+        assert_eq!(0.0, node.y);
 
-        network.add_node(from);
-        network.add_node(to);
-        network.add_link(link);
+        // test the link structure
+        let link = result.links().first().unwrap();
+        assert_eq!("23", link.id);
+        assert_eq!("15", link.from);
+        assert_eq!("1", link.to);
+        assert_eq!(10000.0, link.length);
+        assert_eq!(36000, link.capacity);
+        assert_eq!(27.78, link.freespeed);
+        assert_eq!(1, link.permlanes);
 
-        let result = network.get_link("link-id");
-
-        println!("Link with id: {} and freespeed: {} and from_id: {} and to_id: {}", result.id, result.freespeed, result.from_id, result.to_id);
-    }
-
-    #[test]
-    fn add_multiple_nodes_and_links() {
-        let mut network = Network::new();
-
-        for i in 0..99 {
-            let node = Node { id: String::from(i.to_string()), coord: Coord { x: i as f32, y: i as f32 } };
-            network.add_node(node);
-        }
-
-        for i in 1..98 {
-            let to_id = i - 1;
-            let from_id = i + 1;
-            let link = Link { id: format!("link-{}", i), from_id: String::from(from_id.to_string()), to_id: String::from(to_id.to_string()), freespeed: 100.6 };
-            network.add_link(link);
-        }
-
-        println!("Nodes:");
-        for index in network.graph.node_indices() {
-            let node = network.graph.node_weight(index).unwrap();
-            print!("{}, ", node.id)
-        }
-
-        println!("\nLinks: ");
-        for index in network.graph.edge_indices() {
-            let link = network.graph.edge_weight(index).unwrap();
-            print!("{}, ", link.id)
-        }
+        Ok(())
     }
 }
