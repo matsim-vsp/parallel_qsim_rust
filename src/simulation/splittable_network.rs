e2d9328a60b828702703d5153be6bf5b8c2c00b8
Janek Laudan <laudan@tu-berlin.de>
2022-06-24 17:55:48 +0200

    somehow split the network

--- src/simulation/splittable_network.rs
+++ src/simulation/splittable_network.rs
@@ -1,24 +1,25 @@
 use crate::container::network::{IOLink, IONetwork, IONode};
 use crate::simulation::flow_cap::Flowcap;
 use std::collections::{HashMap, VecDeque};
-use std::hash::Hash;
 
 use crate::simulation::q_vehicle::QVehicle;
 
-pub struct Network<'a> {
-    pub links: HashMap<usize, Link>,
-    pub nodes: HashMap<usize, Node>,
-    pub link_id_mapping: HashMap<&'a str, usize>,
-    pub node_id_mapping: HashMap<&'a str, usize>,
+pub struct IdMappings {
+    node_2_thread: HashMap<usize, usize>,
+    link_2_thread: HashMap<usize, usize>,
 }
 
-impl<'a> Network<'a> {
+#[derive(Debug)]
+pub struct Network {
+    pub links: HashMap<usize, Link>,
+    pub nodes: HashMap<usize, Node>,
+}
+
+impl Network {
     fn new() -> Self {
         Self {
             links: HashMap::new(),
             nodes: HashMap::new(),
-            link_id_mapping: HashMap::new(),
-            node_id_mapping: HashMap::new(),
         }
     }
 
@@ -26,13 +27,19 @@ impl<'a> Network<'a> {
         container: &IONetwork,
         size: usize,
         splitter: fn(&IONode) -> usize,
-    ) -> Vec<Network> {
+    ) -> (Vec<Network>, IdMappings) {
         // create the result networks which can then be populated
         let mut result = Vec::with_capacity(size);
-        let mut global_node_id_mapping: HashMap<&str, usize> = HashMap::new();
-        let mut id_2_thread: HashMap<usize, usize> = HashMap::new();
 
-        for i in 0..size {
+        let mut node_id_mapping: HashMap<&str, usize> = HashMap::new();
+        let mut link_id_mapping: HashMap<&str, usize> = HashMap::new();
+
+        let mut id_mapping = IdMappings {
+            link_2_thread: HashMap::new(),
+            node_2_thread: HashMap::new(),
+        };
+
+        for _i in 0..size {
             result.push(Network::new());
         }
 
@@ -41,49 +48,53 @@ impl<'a> Network<'a> {
             let thread_id = splitter(node);
             let network = result.get_mut(thread_id).unwrap();
 
-            network.add_node(&node.id, next_id);
-            global_node_id_mapping.insert(&node.id, next_id);
+            network.add_node(next_id);
+            node_id_mapping.insert(&node.id, next_id);
+            id_mapping.node_2_thread.insert(next_id, thread_id);
             next_id = next_id + 1;
         }
 
         next_id = 0;
         for link in container.links() {
-            let from_id = *global_node_id_mapping.get(link.from.as_str()).unwrap();
-            let to_id = *global_node_id_mapping.get(link.to.as_str()).unwrap();
+            let from_id = *node_id_mapping.get(link.from.as_str()).unwrap();
+            let to_id = *node_id_mapping.get(link.to.as_str()).unwrap();
 
-            let from_thread = id_2_thread.get(&from_id).unwrap();
-            let to_thread = id_2_thread.get(&to_id).unwrap();
+            let from_thread = *id_mapping.node_2_thread.get(&from_id).unwrap();
+            let to_thread = *id_mapping.node_2_thread.get(&to_id).unwrap();
 
             if from_thread == to_thread {
-                let network = result.get_mut(*from_thread).unwrap();
-                network.add_link(link, next_id, from_id, to_id)
+                let network = result.get_mut(from_thread).unwrap();
+                network.add_local_link(link, next_id, from_id, to_id);
             } else {
-                panic!(
-                    "Not yet implemented. Think about what to do, if link crosses thread boundary"
-                )
-            }
+                let from_network = result.get_mut(from_thread).unwrap();
+                from_network.add_split_out_link(next_id, from_id, from_thread, to_thread);
 
+                let to_network = result.get_mut(to_thread).unwrap();
+                to_network.add_split_in_link(link, next_id, to_id);
+            }
+            link_id_mapping.insert(&link.id, next_id);
+            // the link is associated with the network which contains its to-node
+            id_mapping.link_2_thread.insert(next_id, to_thread);
             next_id = next_id + 1;
         }
 
-        result
-    }
-    fn add_node(&mut self, node_id: &'a str, id: usize) {
-        let node = Node::new(id);
-        self.nodes.insert(id, node);
-        self.node_id_mapping.insert(node_id, id);
+        (result, id_mapping)
     }
 
-    fn add_link(&mut self, link: &'a IOLink, id: usize, from: usize, to: usize) {
-        let new_link = Link {
+    fn add_node(&mut self, id: usize) {
+        let node = Node::new(id);
+        self.nodes.insert(id, node);
+    }
+
+    fn add_local_link(&mut self, link: &IOLink, id: usize, from: usize, to: usize) {
+        let new_link = LocalLink {
             id,
             q: VecDeque::new(),
             flowcap: Flowcap::new(link.capacity),
             freespeed: link.freespeed,
             length: link.length,
         };
-        self.links.insert(id, new_link);
-        self.link_id_mapping.insert(&link.id, id);
+        self.links.insert(id, Link::LocalLink(new_link));
 
         // wire up the from and to node
         let from = self.nodes.get_mut(&from).unwrap();
@@ -91,8 +102,37 @@ impl<'a> Network<'a> {
         let to = self.nodes.get_mut(&to).unwrap();
         to.in_links.push(id);
     }
+
+    fn add_split_out_link(&mut self, id: usize, from: usize, from_thread: usize, to_thread: usize) {
+        let new_link = SplitLink {
+            id,
+            from_thread_id: from_thread,
+            to_thread_id: to_thread,
+        };
+        self.links.insert(id, Link::SplitLink(new_link));
+
+        // wire up from node
+        let from_node = self.nodes.get_mut(&from).unwrap();
+        from_node.out_links.push(id);
+    }
+
+    fn add_split_in_link(&mut self, link: &IOLink, id: usize, to: usize) {
+        let new_link = LocalLink {
+            id,
+            q: VecDeque::new(),
+            flowcap: Flowcap::new(link.capacity),
+            freespeed: link.freespeed,
+            length: link.length,
+        };
+        self.links.insert(id, Link::LocalLink(new_link));
+
+        // wire up to node
+        let to_node = self.nodes.get_mut(&to).unwrap();
+        to_node.in_links.push(id);
+    }
 }
 
+#[derive(Debug)]
 pub struct Node {
     id: usize,
     in_links: Vec<usize>,
@@ -109,7 +149,14 @@ impl Node {
     }
 }
 
-pub struct Link {
+#[derive(Debug)]
+pub enum Link {
+    LocalLink(LocalLink),
+    SplitLink(SplitLink),
+}
+
+#[derive(Debug)]
+pub struct LocalLink {
     id: usize,
     q: VecDeque<QVehicle>,
     length: f32,
@@ -117,13 +164,43 @@ pub struct Link {
     flowcap: Flowcap,
 }
 
-pub struct Id {
-    thread: usize,
+#[derive(Debug)]
+pub struct SplitLink {
     id: usize,
+    from_thread_id: usize,
+    to_thread_id: usize,
 }
 
-impl Id {
-    fn new(thread: usize, id: usize) -> Id {
-        Id { thread, id }
+#[cfg(test)]
+mod tests {
+    use crate::container::network::{IONetwork, IONode};
+    use crate::simulation::splittable_network::Network;
+
+    #[test]
+    fn from_container() {
+        let io_network = IONetwork::from_file("./assets/equil-network.xml");
+        let (split_networks, id_mappings) = Network::split_from_container(&io_network, 2, split);
+
+        assert_eq!(split_networks.len(), 2);
+
+        let first = split_networks.get(0).unwrap();
+        assert_eq!(first.nodes.len(), 8);
+        assert_eq!(first.links.len(), 17);
+
+        let second = split_networks.get(1).unwrap();
+        assert_eq!(second.nodes.len(), 7);
+        assert_eq!(second.links.len(), 16);
+
+        assert_eq!(id_mappings.link_2_thread.len(), 23);
+        assert_eq!(id_mappings.node_2_thread.len(), 15);
+    }
+
+    fn split(node: &IONode) -> usize {
+        let node_group_1 = vec!["1", "2", "3", "4", "5", "6", "7", "15"];
+        if node_group_1.contains(&node.id.as_str()) {
+            0
+        } else {
+            1
+        }
     }
 }
