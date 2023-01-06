fa798b0c738468698e8ddf10ee3182cef31cd7b1
Janek Laudan <laudan@tu-berlin.de>
2023-01-06 16:13:00 +0100

    also add build.rs files which generates Types from proto files.

---
+++ build.rs
@@ -0,0 +1,10 @@
+extern crate prost_build;
+fn main() {
+    // we use the protobuf-src which provides the protoc compiler. This line makes it available
+    // to prost-build
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+
+    // this line comes from the prost-build example and compiles items.proto into corresponding types.
+    // the generated code is under ./target/<goal, e.g. debug>/build/<project-name>-<some-hash>/out
+    prost_build::compile_protos(&["src/mpi/messages.proto"], &["src/"]).unwrap();
+}
