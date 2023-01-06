0c6e6a77b1f951cd49286839e39f49a0ec69d4fe
Janek Laudan <laudan@tu-berlin.de>
2023-01-06 10:29:29 +0100

    create simple send/receive ping pong example

--- src/bin/simple_mpi.rs
+++ src/bin/simple_mpi.rs
@@ -1,14 +1,41 @@
-use mpi::traits::Communicator;
+use mpi::traits::{Communicator, CommunicatorCollectives, Destination, Source};
+use mpi::Rank;
 
 fn main() {
     let universe = mpi::initialize().unwrap();
-
-    let bla = mpi::environment::version();
-    println!("MPI version: {:?}", bla);
     let world = universe.world();
-    println!(
-        "Hello parallel world from process {} of {}!",
-        world.rank(),
-        world.size()
+    let rank = world.rank();
+    let size = world.size();
+
+    assert_eq!(
+        2, size,
+        "This example is supposed to be run with two processes. (mpirun -np 2...)"
     );
+    let other_process = match rank {
+        0 => 1,
+        1 => 0,
+        _ => {
+            panic!("This shouldn't happen :-(")
+        }
+    };
+    let mut counter = 0;
+
+    // start the ping pong
+    if rank == 0 {
+        println!("Process #{rank} sending {counter}");
+        world.process_at_rank(1).send(&counter);
+    }
+
+    // play ping pong
+    while counter < 10 {
+        let (message, status) = world.any_process().receive::<i32>();
+        println!("Process #{rank} received {message}");
+        counter = message + 1;
+        println!("Process #{rank} sending {counter}");
+        world.process_at_rank(other_process).send(&counter);
+    }
+
+    println!("Process #{rank} at barrier.");
+    world.barrier();
+    println!("Process #{rank} after barrier. Done.");
 }
