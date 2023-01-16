a4665fa4e85f6117100a7caf2877f04b46a16d9f
Janek Laudan <laudan@tu-berlin.de>
2023-01-16 16:45:25 +0100

    start working on messaging

--- src/mpi/message_broker.rs
+++ src/mpi/message_broker.rs
@@ -1,5 +1,26 @@
+use crate::mpi::messages::proto::Vehicle;
 use mpi::topology::SystemCommunicator;
 
-pub struct MessageBroker {
+pub trait MessageBroker {
+    fn send(&mut self, now: u32);
+    fn receive(&mut self, now: u32);
+    fn add_veh(&mut self, vehicle: Vehicle);
+}
+pub struct MpiMessageBroker {
     communicator: SystemCommunicator,
+    neighbors: Vec<usize>,
+}
+
+impl MessageBroker for MpiMessageBroker {
+    fn send(&mut self, now: u32) {
+        todo!()
+    }
+
+    fn receive(&mut self, now: u32) {
+        todo!()
+    }
+
+    fn add_veh(&mut self, vehicle: Vehicle) {
+        todo!()
+    }
 }
--- src/mpi/messages.rs
+++ src/mpi/messages.rs
@@ -1,4 +1,4 @@
-use crate::mpi::messages::proto::ExperimentalMessage;
+use crate::mpi::messages::proto::{Agent, ExperimentalMessage, Vehicle};
 use prost::Message;
 use std::io::Cursor;
 
@@ -24,5 +24,61 @@ impl ExperimentalMessage {
 
     pub fn deserialize(buf: &[u8]) -> ExperimentalMessage {
         ExperimentalMessage::decode(&mut Cursor::new(buf)).unwrap()
+    }
+}
+
+impl crate::mpi::messages::proto::Vehicle {
+    pub fn new(id: usize, agent: Agent) -> Vehicle {
+        Vehicle {
+            id: id as u64,
+            agent: Some(agent),
+            cur_route_elem: 0,
+        }
+    }
+
+    pub fn advance_route(&mut self) {
+        self.cur_route_elem += 1;
+    }
+}
+
+impl crate::mpi::messages::proto::Agent {
+    pub fn curr_act(&self) -> &crate::mpi::messages::proto::Activity {
+        if self.current_element % 2 != 0 {
+            panic!("Current element is not an activity");
+        }
+        let act_index = self.current_element / 2;
+        self.plan
+            .as_ref()
+            .unwrap()
+            .acts
+            .get(act_index as usize)
+            .unwrap()
+    }
+
+    pub fn curr_leg(&self) -> &proto::Leg {
+        if self.current_element % 2 != 1 {
+            panic!("Current element is not a leg.");
+        }
+
+        let leg_index = (self.current_element - 1) / 2;
+        self.plan
+            .as_ref()
+            .unwrap()
+            .legs
+            .get(leg_index as usize)
+            .unwrap()
+    }
+
+    pub fn advance_plan(&mut self) {
+        let next = self.current_element + 1;
+        if self.plan.as_ref().unwrap().acts.len() + self.plan.as_ref().unwrap().legs.len()
+            == next as usize
+        {
+            panic!(
+                "Agent: Advance plan was called on agent #{}, but no element is remaining.",
+                self.id
+            )
+        }
+        self.current_element = next;
     }
 }
