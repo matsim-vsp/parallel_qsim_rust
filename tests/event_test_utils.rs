86c6ad0c3f39adecdd7f51a5f7a332ab60e5c367
Paul Heinrich <paul.heinrich@outlook.com>
2023-01-22 17:03:55 +0100

    deserialize string to u64

--- tests/event_test_utils.rs
+++ tests/event_test_utils.rs
@@ -1,7 +1,9 @@
 use rust_q_sim::config::Config;
 use rust_q_sim::logging::init_logging;
 use rust_q_sim::{controller, io};
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Deserializer, Serialize};
+use std::str::FromStr;
+use std::usize;
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 pub struct Events {
@@ -30,36 +32,56 @@ enum SimEvent {
     ActivityStart(Activity),
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Default)]
 struct Activity {
-    time: String,
-    person: String,
-    link: String,
+    #[serde(deserialize_with = "str_to_u64")]
+    time: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    person: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    link: u64,
     #[serde(rename = "actType")]
     act_type: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Default)]
 struct ArrivalDeparture {
-    time: String,
-    person: String,
-    link: String,
+    #[serde(deserialize_with = "str_to_u64")]
+    time: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    person: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    link: u64,
     #[serde(rename = "legMode")]
     leg_mode: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Default)]
 struct PersonVehicleInteraction {
-    time: String,
-    person: String,
-    vehicle: String,
+    #[serde(deserialize_with = "str_to_u64")]
+    time: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    person: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    vehicle: u64,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Default)]
 struct LinkInteraction {
-    time: String,
-    link: String,
-    vehicle: String,
+    #[serde(deserialize_with = "str_to_u64")]
+    time: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    link: u64,
+    #[serde(deserialize_with = "str_to_u64")]
+    vehicle: u64,
+}
+
+fn str_to_u64<'de, D>(deserializer: D) -> Result<u64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    u64::from_str(&s).map_err(de::Error::custom)
 }
 
 pub fn run_simulation_and_compare_events(config: Config, path_to_expected_scenario_files: &str) {
