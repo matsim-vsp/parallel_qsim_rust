a9a7e50b9594a52a3b50e6480c27d8e32e572dac
Paul Heinrich <paul.heinrich@outlook.com>
2023-01-23 13:36:09 +0100

    error handling xml reader

--- src/io/xml_reader.rs
+++ src/io/xml_reader.rs
@@ -21,8 +21,11 @@ where
         let result: T = from_reader(buffered_decoder).unwrap();
         result
     } else if file_path.ends_with(".xml") {
-        let result: T = from_reader(buffered_reader).unwrap();
-        return result;
+        let result: Result<T, _> = from_reader(buffered_reader);
+        match result {
+            Ok(x) => x,
+            Err(e) => panic!("Problem reading file: {:?}", e),
+        }
     } else {
         panic!(
             "xml_reader::read: Can't open file path: {}. Only files with endings '.xml' or '.xml.gz' are supported.",
