ad85dcf23ce4410d228d01bfe0fd1d5257c17fc1
Paul Heinrich <paul.heinrich@outlook.com>
2023-06-01 14:34:56 +0200

    handle person leaves vehicle correctly.

--- src/simulation/messaging/travel_time_collector.rs
+++ src/simulation/messaging/travel_time_collector.rs
@@ -1,5 +1,7 @@
 use crate::simulation::messaging::events::proto::event::Type;
-use crate::simulation::messaging::events::proto::{Event, LinkEnterEvent, LinkLeaveEvent};
+use crate::simulation::messaging::events::proto::{
+    Event, LinkEnterEvent, LinkLeaveEvent, PersonLeavesVehicleEvent,
+};
 use crate::simulation::messaging::events::EventsSubscriber;
 use std::any::Any;
 use std::collections::HashMap;
@@ -7,6 +9,7 @@ use std::collections::HashMap;
 pub struct TravelTimeCollector {
     travel_times_by_link: HashMap<u64, Vec<u32>>,
     cache_traffic_information_by_link: HashMap<u64, Vec<TrafficInformation>>,
+    current_link_by_vehicle: HashMap<u64, u64>,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -20,6 +23,7 @@ impl TravelTimeCollector {
         TravelTimeCollector {
             travel_times_by_link: HashMap::new(),
             cache_traffic_information_by_link: HashMap::new(),
+            current_link_by_vehicle: HashMap::new(),
         }
     }
 
@@ -32,6 +36,9 @@ impl TravelTimeCollector {
                 time,
                 vehicle: event.vehicle,
             });
+
+        self.current_link_by_vehicle
+            .insert(event.vehicle, event.link);
     }
 
     fn process_link_leave_event(&mut self, time: u32, event: &LinkLeaveEvent) {
@@ -57,6 +64,30 @@ impl TravelTimeCollector {
                 .or_insert(Vec::new())
                 .push(time - traffic_information.time)
         }
+    }
+
+    fn process_person_leaves_vehicle_event(&mut self, time: u32, event: &PersonLeavesVehicleEvent) {
+        let link_id = self
+            .current_link_by_vehicle
+            .get(&event.vehicle).copied()
+            .expect("Before a person can leave a vehicle, it must have entered the link and thus a current link must be available.");
+
+        let index_of_link_enter = self
+            .get_index_of_link_enter_event(link_id, event.vehicle)
+            .expect("Before a person can leave a vehicle, it must have entered the link and thus traffic information must be available.");
+
+        self.cache_traffic_information_by_link
+            .get_mut(&link_id)
+            .unwrap()
+            .remove(index_of_link_enter);
+    }
+
+    fn get_index_of_link_enter_event(&mut self, link: u64, vehicle: u64) -> Option<usize> {
+        self.cache_traffic_information_by_link
+            .entry(link)
+            .or_insert(Vec::new())
+            .iter()
+            .position(|t| t.vehicle == vehicle)
     }
 
     pub fn get_travel_time_of_link(&self, link: u64) -> Option<u32> {
@@ -92,6 +123,7 @@ impl EventsSubscriber for TravelTimeCollector {
         match event.r#type.as_ref().unwrap() {
             Type::LinkEnter(e) => self.process_link_enter_event(time, e),
             Type::LinkLeave(e) => self.process_link_leave_event(time, e),
+            Type::PersonLeavesVeh(e) => self.process_person_leaves_vehicle_event(time, e),
             _ => {}
         }
     }
@@ -177,6 +209,55 @@ mod test {
                 time: 5,
                 vehicle: 3,
             })
+        );
+    }
+
+    #[test]
+    /// Tests whether PersonLeavesVehicleEvent discards travel time
+    fn test_with_person_leaves_vehicle() {
+        let mut collector = TravelTimeCollector::new();
+        collector.receive_event(0, &Event::new_link_enter(1, 1));
+        collector.receive_event(2, &Event::new_person_leaves_veh(1, 1));
+        collector.receive_event(4, &Event::new_link_leave(1, 1));
+
+        assert_eq!(collector.get_travel_time_of_link(1), None);
+        assert_eq!(
+            collector
+                .cache_traffic_information_by_link
+                .get(&1)
+                .unwrap()
+                .len(),
+            0
+        );
+    }
+
+    #[test]
+    /// Tests whether PersonLeavesVehicleEvent discards travel time
+    fn test_with_person_leaves_vehicle_complex() {
+        let mut collector = TravelTimeCollector::new();
+        collector.receive_event(0, &Event::new_link_enter(1, 1));
+
+        //intermediate veh 2 enters link 1
+        collector.receive_event(1, &Event::new_link_enter(1, 2));
+
+        collector.receive_event(2, &Event::new_person_leaves_veh(1, 1));
+
+        //intermediate veh 2 leaves link 1
+        collector.receive_event(3, &Event::new_link_leave(1, 2));
+
+        collector.receive_event(10, &Event::new_link_leave(1, 1));
+        collector.receive_event(10, &Event::new_link_enter(2, 1));
+        collector.receive_event(20, &Event::new_link_leave(2, 1));
+
+        assert_eq!(collector.get_travel_time_of_link(1), Some(2));
+        assert_eq!(collector.get_travel_time_of_link(2), Some(10));
+        assert_eq!(
+            collector
+                .cache_traffic_information_by_link
+                .get(&1)
+                .unwrap()
+                .len(),
+            0
         );
     }
 }
