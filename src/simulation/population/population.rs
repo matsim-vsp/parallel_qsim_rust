f5600f9b80397cfb23086f2b0bd98e3ecab3c5fb
Janek Laudan <laudan@tu-berlin.de>
2023-06-07 15:37:06 +0200

    add unit testing for population

--- src/simulation/population/population.rs
+++ src/simulation/population/population.rs
@@ -7,6 +7,7 @@ use std::collections::HashMap;
 
 type ActType = ();
 
+#[derive(Debug)]
 pub struct Population<'p> {
     pub agents: HashMap<Id<Agent>, Agent>,
     pub agent_ids: IdStore<'p, Agent>,
@@ -103,5 +104,63 @@ impl<'p> Population<'p> {
             }
             _t => panic!("Unsupported route type: '{_t}'"),
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::simulation::config::RoutingMode;
+    use crate::simulation::messaging::messages::proto::leg::Route;
+    use crate::simulation::network::global_network::Network;
+    use crate::simulation::population::population::Population;
+
+    #[test]
+    fn from_io_1_plan() {
+
+        let net = Network::from_file("./assets/equil/equil-network.xml", 2);
+        let pop = Population::from_file("./assets/equil/equil-1-plan.xml", &net, 1, RoutingMode::UsePlans);
+
+        assert_eq!(1, pop.agents.len());
+
+        let agent = pop.agents.get(&pop.agent_ids.get_from_ext("1")).unwrap();
+        assert!(matches!(&agent.plan, Some(_)));
+
+        let plan = agent.plan.as_ref().unwrap();
+        assert_eq!(4, plan.acts.len());
+        assert_eq!(3, plan.legs.len());
+
+        let home_act = plan.acts.first().unwrap();
+        assert_eq!("h", home_act.act_type.as_str());
+        assert_eq!(net.link_ids.get_from_ext("1").internal as u64, home_act.link_id);
+        assert_eq!(-25000., home_act.x);
+        assert_eq!(0., home_act.y);
+        assert_eq!(Some(6*3600), home_act.end_time);
+        assert_eq!(None, home_act.start_time);
+        assert_eq!(None, home_act.max_dur);
+
+        let leg = plan.legs.first().unwrap();
+        assert_eq!(None, leg.trav_time);
+        assert_eq!(None, leg.dep_time);
+        assert!(matches!(leg.route, Some(_)));
+        if let Route::NetworkRoute(net_route) = leg.route.as_ref().unwrap() {
+            assert_eq!(pop.vehicle_ids.get_from_ext("1").internal as u64, net_route.vehicle_id);
+            assert_eq!(vec![
+                net.link_ids.get_from_ext("1").internal as u64,
+                net.link_ids.get_from_ext("6").internal as u64,
+                net.link_ids.get_from_ext("15").internal as u64,
+                net.link_ids.get_from_ext("20").internal as u64,
+            ], net_route.route);
+        } else {
+            panic!("Expected network route as first leg.")
+        }
+    }
+
+    #[test]
+    fn from_io() {
+
+        let net = Network::from_file("./assets/equil/equil-network.xml", 2);
+        let pop = Population::from_file("./assets/equil/equil-plans.xml.gz", &net, 1, RoutingMode::UsePlans);
+
+        assert_eq!(100, pop.agents.len());
     }
 }
