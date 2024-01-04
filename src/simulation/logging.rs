b452f57aa9bce0cdeec77668984cb546954b85d1
janekdererste <janek.laudan@mailbox.org>
2024-01-04 11:27:14 +0100

    Cleanup monitoring code and use it in loggin.rs

--- src/simulation/logging.rs
+++ src/simulation/logging.rs
@@ -5,9 +5,10 @@ use tracing::level_filters::LevelFilter;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::{non_blocking, rolling};
 use tracing_subscriber::fmt;
-use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::Layer;
+
+use crate::simulation::profiling::{SpanDurationToCSVLayer, WriterGuard};
 
 pub fn init_std_out_logging() {
     let collector = tracing_subscriber::registry().with(
@@ -18,21 +19,29 @@ pub fn init_std_out_logging() {
     tracing::subscriber::set_global_default(collector).expect("Unable to set a global collector");
 }
 
-pub fn init_logging(dir: &Path, file_discriminant: &str) -> (WorkerGuard, WorkerGuard) {
+pub fn init_logging(dir: &Path, file_discriminant: &str) -> (WorkerGuard, WriterGuard) {
     let log_file_name = format!("log_process_{file_discriminant}.txt");
-    let log_file_appender = rolling::never(dir, &log_file_name);
+    let log_file_appender = rolling::never(dir, log_file_name);
     let (log_file, _guard_log) = non_blocking(log_file_appender);
 
-    let trace_dir = dir.join("trace");
+    /*let trace_dir = dir.join("trace");
     let trace_file_name = format!("trace_process_{file_discriminant}.txt");
     let trace_file_appender = rolling::never(&trace_dir, &trace_file_name);
     let (trace_file, _guard_performance) = non_blocking(trace_file_appender);
 
+     */
+
+    let duration_dir = dir.join("instrument");
+    let duration_file_name = format!("instrument_process_{file_discriminant}.csv");
+    let duration_path = duration_dir.join(duration_file_name);
+    let (csv_layer, _guard) = SpanDurationToCSVLayer::new(&duration_path);
+
     let collector = tracing_subscriber::registry()
+        .with(csv_layer)
         .with(
             fmt::Layer::new()
-                .with_span_events(FmtSpan::CLOSE)
                 .with_writer(io::stdout)
+                .pretty()
                 .with_filter(LevelFilter::INFO),
         )
         .with(
@@ -41,15 +50,17 @@ pub fn init_logging(dir: &Path, file_discriminant: &str) -> (WorkerGuard, Worker
                 .json()
                 .with_ansi(false)
                 .with_filter(LevelFilter::DEBUG),
-        )
-        .with(
-            fmt::Layer::new()
-                .with_writer(trace_file)
-                .with_span_events(FmtSpan::CLOSE)
-                .with_ansi(false)
-                .json()
-                .with_filter(LevelFilter::TRACE),
         );
+    /*.with(
+        fmt::Layer::new()
+            .with_writer(trace_file)
+            .with_span_events(FmtSpan::CLOSE)
+            .with_ansi(false)
+            .json()
+            .with_filter(LevelFilter::TRACE),
+    );
+
+     */
     tracing::subscriber::set_global_default(collector).expect("Unable to set a global collector");
-    (_guard_log, _guard_performance)
+    (_guard_log, _guard)
 }
--- src/simulation/profiling/mod.rs
+++ src/simulation/profiling/mod.rs
@@ -1,17 +1,13 @@
-use std::fmt::Debug;
-use std::fmt::Write;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Arc, Mutex};
 use std::time::{Instant, SystemTime};
 use std::{env, fs};
 
 use serde_json::{json, Value};
-use tracing::field::{Field, FieldSet, Visit};
 use tracing::span::Attributes;
-use tracing::{field, trace, Event, Id, Subscriber};
-use tracing_subscriber::fmt::{format, FmtContext, FormatEvent, FormatFields, MakeWriter};
+use tracing::{trace, Id, Subscriber};
 use tracing_subscriber::layer::Context;
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::Layer;
@@ -48,64 +44,81 @@ pub fn measure_duration<Out, F: FnOnce() -> Out>(
     res
 }
 
-struct CustomLayer {
+pub struct SpanDurationToCSVLayer {
     writer: Arc<Mutex<BufWriter<File>>>,
 }
 
-impl CustomLayer {
-    fn new(path: &Path) -> Self {
-        let header = "timestamp,target,func_name,elapsed_time,\n".to_string();
+pub struct WriterGuard {
+    writer_ref: Arc<Mutex<BufWriter<File>>>,
+}
+
+struct SpanDuration {
+    elapsed: u64,
+    last: Instant,
+}
+
+impl SpanDurationToCSVLayer {
+    pub fn new(path: &Path) -> (Self, WriterGuard) {
+        // create necessary file path and corresponding file wrapped in buffered writer
+        let prefix = path.parent().unwrap();
+        fs::create_dir_all(prefix).unwrap();
         let file =
-            File::create(path).unwrap_or_else(|_e| panic!("Unable to create file at {:?}", path));
+            File::create(path).unwrap_or_else(|_e| panic!("Failed to open file at: {path:?}"));
         let mut writer = BufWriter::new(file);
-        std::io::Write::write(&mut writer, header.as_bytes()).expect("Failed to write header.");
-        Self {
-            writer: Arc::new(Mutex::new(writer)),
-        }
+
+        // write header for csv file
+        std::io::Write::write(
+            &mut writer,
+            "timestamp,target,func_name,duration\n".as_bytes(),
+        )
+        .unwrap_or_else(|_e| panic!("Failed to write header."));
+
+        // wrap the writer into an arc<mutex<...>> so that we can keep a reference which gets dropped
+        // at the end of the scope calling this method. The mutex is necessary, because the Layer
+        // must be Sync + Send for the tracing_subscriber subscriber
+        let writer_ref = Arc::new(Mutex::new(writer));
+        let new_self = Self {
+            writer: writer_ref.clone(),
+        };
+        let guard = WriterGuard { writer_ref };
+        (new_self, guard)
     }
 
-    fn write_metadata(e: &Event) -> String {
-        format!(
+    fn write_metadata(writer: &mut BufWriter<File>, m: &tracing::Metadata) {
+        // import Write here, to avoid conflicts with std::fmt::Write
+        use std::io::Write;
+
+        write!(
+            writer,
             "{},{},{},",
             SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap()
                 .as_nanos(),
-            e.metadata().target(),
-            e.metadata().name()
+            m.target(),
+            m.name(),
         )
+        .unwrap();
     }
 }
 
-impl<S> Layer<S> for CustomLayer
+/// Simple Layer implementation, which records the time elapsed between a a span being opened and being
+/// closed again. Once a span is closed, it writes the elapsed time into a csv journal
+impl<S> Layer<S> for SpanDurationToCSVLayer
 where
     S: Subscriber + for<'a> LookupSpan<'a>,
 {
-    fn on_new_span(&self, attrs: &Attributes<'_>, id: &Id, ctx: Context<'_, S>) {
+    fn on_new_span(&self, _attrs: &Attributes<'_>, id: &Id, ctx: Context<'_, S>) {
         let span = ctx.span(id).expect("should exist");
         let mut extensions = span.extensions_mut();
-
-        println!("{attrs:?}");
-        extensions.insert(CustomTimings::new());
-    }
-
-    fn on_event(&self, event: &Event, _ctx: Context<'_, S>) {
-        let result = Self::write_metadata(event);
-        let mut visitor = CustomVisitor { result };
-        event.record(&mut visitor as &mut dyn Visit);
-        visitor.result.push('\n');
-        println!("Writing: {}", visitor.result);
-
-        let mut bla = self.writer.lock().unwrap();
-        std::io::Write::write(&mut *bla, visitor.result.as_bytes()).expect("");
-        // bla.write(visitor.result.as_bytes()).expect("TODO: panic message");
+        extensions.insert(SpanDuration::new());
     }
 
     fn on_enter(&self, id: &Id, ctx: Context<'_, S>) {
         let span = ctx.span(id).expect("Should exist");
         let mut extensions = span.extensions_mut();
 
-        if let Some(timing) = extensions.get_mut::<CustomTimings>() {
+        if let Some(timing) = extensions.get_mut::<SpanDuration>() {
             timing.last = Instant::now();
         }
     }
@@ -114,197 +127,46 @@ where
         let span = ctx.span(id).expect("Span should be there");
         let mut extensions = span.extensions_mut();
 
-        if let Some(timing) = extensions.get_mut::<CustomTimings>() {
+        if let Some(timing) = extensions.get_mut::<SpanDuration>() {
             let now = Instant::now();
-            timing.busy += (now - timing.last).as_nanos() as u64;
+            timing.elapsed += (now - timing.last).as_nanos() as u64;
         }
     }
 
     fn on_close(&self, id: Id, ctx: Context<'_, S>) {
-        println!("Start close");
+        use std::io::Write;
+
         let span = ctx.span(&id).expect("Span should be there!");
         let extensions = span.extensions();
-        let callsite = span.metadata().callsite();
         let meta = span.metadata();
-        let fs = FieldSet::new(&["time", "custom close"], callsite);
 
-        let time = extensions.get::<CustomTimings>().unwrap();
-        let v = [
-            (
-                &fs.field("time").unwrap(),
-                Some(&time.busy as &dyn field::Value),
-            ),
-            (
-                &fs.field("custom close").unwrap(),
-                Some(&"custom close message" as &dyn field::Value),
-            ),
-        ];
-        let value_set = fs.value_set(&v);
-        Event::child_of(id, meta, &value_set);
+        let writer = &mut *self.writer.lock().unwrap();
+        Self::write_metadata(writer, meta);
 
+        let span_duration = extensions.get::<SpanDuration>().unwrap();
+        write!(writer, "{},", span_duration.elapsed).unwrap();
+        writeln!(writer).unwrap();
+
+        // extensions and span must be dropped explicitly, says the tracing documentation
         drop(extensions);
         drop(span);
-        println!("End close");
     }
 }
 
-impl Drop for CustomLayer {
+impl Drop for WriterGuard {
     fn drop(&mut self) {
-        println!("Custom layer gets dropped.");
-        let mut writer = self.writer.lock().unwrap();
+        println!("Writer guard gets dropped.");
+        let mut writer = self.writer_ref.lock().unwrap();
         std::io::Write::flush(&mut *writer).expect("TODO: panic message");
     }
 }
 
-struct CustomTimings {
-    busy: u64,
-    last: Instant,
-}
-
-impl CustomTimings {
+impl SpanDuration {
     fn new() -> Self {
         Self {
-            busy: 0,
+            elapsed: 0,
             last: Instant::now(),
         }
-    }
-}
-
-struct CustomVisitor {
-    result: String,
-}
-
-impl Visit for CustomVisitor {
-    fn record_debug(&mut self, field: &Field, value: &dyn Debug) {
-        if field.name().eq("time") {
-            write!(&mut self.result, "{:?},", value).unwrap();
-        }
-    }
-}
-
-struct CustomWriter {
-    inner: Arc<Mutex<BufWriter<File>>>,
-}
-
-struct CustomWriterGuard {
-    writer_ref: Arc<Mutex<BufWriter<File>>>,
-}
-
-impl Drop for CustomWriterGuard {
-    fn drop(&mut self) {
-        println!("Starting drop of Custom Writer Guard");
-        let mut locked = self.writer_ref.lock().unwrap();
-        std::io::Write::flush(&mut *locked)
-            .unwrap_or_else(|e| panic!("failed to flush writer on drop"));
-    }
-}
-
-impl CustomWriter {
-    fn new(path: &Path) -> (Self, CustomWriterGuard) {
-        // create necessary file path and corresponding file wrapped in buffered writer
-        let prefix = path.parent().unwrap();
-        fs::create_dir_all(prefix).unwrap();
-        let file =
-            File::create(path).unwrap_or_else(|e| panic!("Failed to open file at: {path:?}"));
-        let mut writer = BufWriter::new(file);
-
-        // write header for csv file
-        std::io::Write::write(
-            &mut writer,
-            "timestamp,target,func_name,duration\n".as_bytes(),
-        )
-        .unwrap_or_else(|e| panic!("Failed to write header."));
-
-        // wire up stuff, so that we can have a guard and so that we are sync and send, because that
-        // is required by the tracing_subcriber crate
-        let inner = Arc::new(Mutex::new(writer));
-        let result = Self {
-            inner: inner.clone(),
-        };
-        let guard = CustomWriterGuard {
-            writer_ref: result.inner.clone(),
-        };
-        (result, guard)
-    }
-}
-
-struct MakeWriteResult<'a>(MutexGuard<'a, BufWriter<File>>);
-
-impl<'a> MakeWriter<'a> for CustomWriter {
-    type Writer = MakeWriteResult<'a>;
-
-    fn make_writer(&'a self) -> Self::Writer {
-        let locked = self.inner.lock().unwrap();
-        MakeWriteResult(locked)
-    }
-}
-
-impl std::io::Write for MakeWriteResult<'_> {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.0.write(buf)
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.0.flush()
-    }
-}
-
-struct CustomFormatter;
-
-struct CustomTimeVisitor<'a> {
-    writer: &'a mut dyn Write,
-}
-
-impl<'a> CustomTimeVisitor<'a> {
-    fn new(writer: &'a mut dyn Write) -> Self {
-        Self { writer }
-    }
-}
-
-impl<'a> Visit for CustomTimeVisitor<'a> {
-    fn record_u64(&mut self, field: &Field, value: u64) {
-        println!("Visitor record u64");
-        if field.name().eq("time.busy") {
-            write!(&mut self.writer, "{value}")
-                .unwrap_or_else(|e| panic!("Failed to write field time."));
-        }
-    }
-
-    fn record_debug(&mut self, field: &Field, value: &dyn Debug) {
-        println!("Visitor record debug");
-        if field.name().eq("time.busy") {
-            write!(&mut self.writer, "{value:?}")
-                .unwrap_or_else(|e| panic!("Failed to write field time."));
-        }
-    }
-}
-
-impl<S, N> FormatEvent<S, N> for CustomFormatter
-where
-    S: Subscriber + for<'a> LookupSpan<'a>,
-    N: for<'a> FormatFields<'a> + 'static,
-{
-    fn format_event(
-        &self,
-        _ctx: &FmtContext<'_, S, N>,
-        mut writer: format::Writer<'_>,
-        e: &Event<'_>,
-    ) -> std::fmt::Result {
-        write!(
-            &mut writer,
-            "{},{},{},",
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_nanos(),
-            e.metadata().target(),
-            e.metadata().name()
-        )
-        .unwrap_or_else(|e| panic!("OH no! Can't write."));
-
-        let mut visitor = CustomTimeVisitor::new(&mut writer);
-        e.record(&mut visitor);
-        writeln!(&mut writer)
     }
 }
 
@@ -314,34 +176,20 @@ mod tests {
     use std::thread::sleep;
     use std::time::Duration;
 
-    use tracing::{info, instrument, Level};
-    use tracing_subscriber::filter::FilterExt;
-    use tracing_subscriber::fmt::format::FmtSpan;
+    use tracing::{info, instrument};
+    use tracing_subscriber::fmt::Layer;
     use tracing_subscriber::layer::SubscriberExt;
-    use tracing_subscriber::{fmt, EnvFilter, Layer};
 
-    use crate::simulation::profiling::{CustomFormatter, CustomWriter};
+    use crate::simulation::profiling::SpanDurationToCSVLayer;
 
     #[test]
     fn test_events() {
-        let path = PathBuf::from("./test_output/simulation/profiling/test_events");
-        let (writer, guard) = CustomWriter::new(&path);
-        let custom_formatter = CustomFormatter {};
-        let custom_filter = EnvFilter::from_default_env().add_directive(Level::TRACE.into());
-        let custom_filter_2 = EnvFilter::from_default_env()
-            .add_directive(Level::INFO.into())
-            .not();
+        let path = PathBuf::from("./test_output/simulation/profiling/test_events.csv");
 
-        // let custom_layer = CustomLayer::new(&path);
-        // let writer_ref = custom_layer.writer.clone();
-        let layers = tracing_subscriber::registry().with(
-            fmt::Layer::new()
-                .with_span_events(FmtSpan::CLOSE)
-                .with_writer(writer)
-                .event_format(custom_formatter)
-                .with_filter(custom_filter)
-                .with_filter(custom_filter_2),
-        );
+        let (csv_layer, _guard) = SpanDurationToCSVLayer::new(&path);
+        let layers = tracing_subscriber::registry()
+            .with(csv_layer)
+            .with(Layer::new().pretty());
         tracing::subscriber::set_global_default(layers).expect("TODO: panic message");
 
         info!("Before func");
