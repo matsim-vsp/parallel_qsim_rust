9f10c49cce25a170d98f1442f06213f7ecf2c82f
Paul Heinrich <pheinrich11235@gmail.com>
2024-06-17 11:53:37 +0200

    remove Engine trait

--- src/simulation/engines/mod.rs
+++ src/simulation/engines/mod.rs
@@ -1,28 +1,22 @@
+use crate::simulation::engines::activity_engine::ActivityEngine;
+use crate::simulation::engines::leg_engine::LegEngine;
+use crate::simulation::messaging::communication::communicators::SimCommunicator;
 use crate::simulation::population::population_data::State;
 use crate::simulation::wire_types::population::Person;
 use std::cell::RefCell;
-use std::rc::{Rc, Weak};
+use std::rc::Rc;
 
 pub mod activity_engine;
 pub mod leg_engine;
 pub mod network_engine;
 pub mod teleportation_engine;
 
-pub trait Engine {
-    fn do_step(&mut self, now: u32);
-    fn receive_agent(&mut self, now: u32, agent: Person);
-    fn set_agent_state_transition_logic(
-        &mut self,
-        internal_interface: Weak<RefCell<AgentStateTransitionLogic>>,
-    );
+pub struct AgentStateTransitionLogic<C: SimCommunicator> {
+    activity_engine: Rc<RefCell<ActivityEngine<C>>>,
+    teleportation_engine: Rc<RefCell<LegEngine<C>>>,
 }
 
-pub struct AgentStateTransitionLogic {
-    activity_engine: Rc<RefCell<dyn Engine>>,
-    teleportation_engine: Rc<RefCell<dyn Engine>>,
-}
-
-impl AgentStateTransitionLogic {
+impl<C: SimCommunicator + 'static> AgentStateTransitionLogic<C> {
     fn arrange_next_agent_state(&self, now: u32, agent: Person) {
         match agent.state() {
             State::ACTIVITY => self.activity_engine.borrow_mut().receive_agent(now, agent),
@@ -34,8 +28,8 @@ impl AgentStateTransitionLogic {
     }
 
     pub(crate) fn new(
-        activity_engine: Rc<RefCell<dyn Engine>>,
-        teleportation_engine: Rc<RefCell<dyn Engine>>,
+        activity_engine: Rc<RefCell<ActivityEngine<C>>>,
+        teleportation_engine: Rc<RefCell<LegEngine<C>>>,
     ) -> Self {
         AgentStateTransitionLogic {
             activity_engine,
