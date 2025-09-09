453c849b7b2c7b5de3ffbb8686f2753b2f2e473b
frievoe97 <friedrich.voelkers@me.com>
2025-09-09 09:18:02 +0200

    restructure url test. remove the simulation and check only if the files are loaded with content

--- tests/test_3_links_url.rs
+++ tests/test_3_links_url.rs
@@ -1,43 +1,35 @@
-use crate::test_simulation::TestExecutorBuilder;
-use rust_q_sim::simulation::config::CommandLineArgs;
-use rust_q_sim::simulation::id::store_to_file;
 use rust_q_sim::simulation::network::Network;
 use rust_q_sim::simulation::population::Population;
 use rust_q_sim::simulation::vehicles::garage::Garage;
 use std::path::PathBuf;
 
-mod test_simulation;
-
 const BASE_URL: &str =
     "https://raw.githubusercontent.com/matsim-vsp/parallel_qsim_rust/refs/heads/43-load-files-via-url/tests/resources/3-links-url";
 
-fn create_resources(out_dir: &PathBuf) {
+#[test]
+fn load_files_from_url_have_content() {
     let input_dir = PathBuf::from(BASE_URL);
 
+    // Load network and check if it contains nodes and links
     let net = Network::from_file_as_is(&input_dir.join("3-links-network.xml"));
+    assert!(
+        !net.nodes().is_empty() && !net.links().is_empty(),
+        "Network should contain nodes and links"
+    );
+
+    // Load vehicles and check if there is at least one vehicle type
     let mut garage = Garage::from_file(&input_dir.join("vehicles.xml"));
+    assert!(
+        !garage.vehicle_types.is_empty(),
+        "Vehicles file should define at least one vehicle type"
+    );
+
+    // Load population and check if there is at least one person
     let pop = Population::from_file(&input_dir.join("1-agent-full-leg.xml"), &mut garage);
-
-    store_to_file(&out_dir.join("ids.binpb"));
-    net.to_file(&out_dir.join("3-links-network.binpb"));
-    pop.to_file(&out_dir.join("1-agent-full-leg.binpb"));
-    garage.to_file(&out_dir.join("vehicles.binpb"));
-}
-
-#[test]
-fn execute_3_links_single_part_from_url() {
-    let test_dir = PathBuf::from("./test_output/simulation/execute_3_links_single_part_from_url/");
-    create_resources(&test_dir);
-
-    let config_url = format!("{}/3-links-config-1.yml", BASE_URL);
-    let events_url = format!("{}/expected_events.xml", BASE_URL);
-
-    let config_args = CommandLineArgs::new_with_path(&config_url);
-
-    TestExecutorBuilder::default()
-        .config_args(config_args)
-        .expected_events(Some(&events_url))
-        .build()
-        .unwrap()
-        .execute();
+    assert!(
+        !pop.persons.is_empty(),
+        "Population should contain at least one person"
+    );
+    
+    // TODO: Check if the expected events file can be loaded and has content
 }
