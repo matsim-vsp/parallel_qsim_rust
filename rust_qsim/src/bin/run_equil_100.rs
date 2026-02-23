9725c73207dffa9d4663929597f5f48b1acadc03
Friedrich Völkers <info@friedrichvoelkers.de>
2026-02-23 13:10:14 +0100

    added test event subscriber to print the events

--- rust_qsim/src/bin/run_equil_100.rs
+++ rust_qsim/src/bin/run_equil_100.rs
@@ -1,6 +1,8 @@
 use rust_qsim::simulation::config::{CommandLineArgs, Config};
 use rust_qsim::simulation::controller::local_controller::LocalControllerBuilder;
+use rust_qsim::simulation::events::print_events::PrintEvents;
 use rust_qsim::simulation::scenario::Scenario;
+use std::collections::HashMap;
 use std::sync::Arc;
 
 fn main() {
@@ -8,8 +10,12 @@ fn main() {
         CommandLineArgs::new_with_path("rust_qsim/assets/equil-100/run_equil_100.config.yml");
     let config = Config::from(args);
 
+    let mut subscribers = HashMap::new();
+    subscribers.insert(0, vec![PrintEvents::register()]);
+
     LocalControllerBuilder::default()
         .scenario(Scenario::load(Arc::new(config)))
+        .events_subscriber_per_partition(subscribers)
         .build()
         .unwrap()
         .run();
