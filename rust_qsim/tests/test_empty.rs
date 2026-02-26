d9ad3c6a200cf1a9a4733ccfd41c31371bb7bcf7
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-02-26 12:41:44 +0100

    test: update `test_empty` to specify custom output directory

--- rust_qsim/tests/test_empty.rs
+++ rust_qsim/tests/test_empty.rs
@@ -7,7 +7,8 @@ use std::sync::Arc;
 /// Test that with default structs the simulation runs without errors
 #[integration_test(rust_qsim)]
 fn test_empty() {
-    let config = Config::default();
+    let mut config = Config::default();
+    config.output_mut().output_dir = "./test_output/simulation/empty".parse().unwrap();
     let scenario = Scenario::load(Arc::new(config));
     let controller = ControllerBuilder::default_with_scenario(scenario)
         .build()
