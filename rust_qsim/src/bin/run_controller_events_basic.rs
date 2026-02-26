b9fc4a9ffa5cd5cb719f812752e7dafc6f75bfbe
Friedrich Völkers <info@friedrichvoelkers.de>
2026-02-26 15:24:05 +0100

    replace BeforeBodsim with BeforeSimStep

--- rust_qsim/src/bin/run_controller_events_basic.rs
+++ rust_qsim/src/bin/run_controller_events_basic.rs
@@ -1,9 +1,10 @@
 use rust_qsim::simulation::config::{CommandLineArgs, Config};
 use rust_qsim::simulation::controller::controller::ControllerBuilder;
 use rust_qsim::simulation::events::controller_event_printer::{
-    BeforeMobsimMessage, ControllerEventPrinter,
+    BeforeSimStepMessage, ControllerEventPrinter,
 };
 use rust_qsim::simulation::scenario::Scenario;
+use std::collections::HashMap;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -12,11 +13,15 @@ fn main() {
     let args =
         CommandLineArgs::new_with_path("rust_qsim/assets/equil-100/run_equil_100.config.yml");
     let config = Config::from(args);
-    let (event_tx, _) = mpsc::channel::<BeforeMobsimMessage>();
+    let (event_tx, _event_rx) = mpsc::channel::<BeforeSimStepMessage>();
+    let mobsim_listener_register_fns = HashMap::from([(
+        0,
+        vec![ControllerEventPrinter::register_fn(event_tx)],
+    )]);
 
     let sim_thread = thread::spawn(move || {
         ControllerBuilder::default_with_scenario(Scenario::load(Arc::new(config)))
-            .controller_event_register_fn(vec![ControllerEventPrinter::register_fn(event_tx)])
+            .mobsim_event_register_fn(mobsim_listener_register_fns)
             .build()
             .unwrap()
             .run();
--- rust_qsim/src/simulation/events/controller_event_printer.rs
+++ rust_qsim/src/simulation/events/controller_event_printer.rs
@@ -1,31 +1,37 @@
 use crate::simulation::framework_events::{
-    ControllerEvent, ControllerEventsManager, ControllerListenerRegisterFn,
+    EventOrigin, MobsimEvent, MobsimEventsManager, MobsimListenerRegisterFn,
 };
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 #[derive(Debug, Clone)]
-pub struct BeforeMobsimMessage {
+pub struct BeforeSimStepMessage {
+    pub partition: u32,
     pub iteration: u32,
     pub seq_no: u64,
-    pub last_iteration: bool,
+    pub time: u32,
 }
 
 pub struct ControllerEventPrinter;
 
 impl ControllerEventPrinter {
     pub fn register_fn(
-        sender: mpsc::Sender<BeforeMobsimMessage>,
-    ) -> Box<ControllerListenerRegisterFn> {
-        Box::new(move |events: &mut ControllerEventsManager| {
+        sender: mpsc::Sender<BeforeSimStepMessage>,
+    ) -> Box<MobsimListenerRegisterFn> {
+        Box::new(move |events: &mut MobsimEventsManager| {
             events.on_event(move |runtime_event| {
-                if let ControllerEvent::BeforeMobsim(event) = &runtime_event.payload {
-                    thread::sleep(Duration::from_secs(10));
-                    let _ = sender.send(BeforeMobsimMessage {
+                if let MobsimEvent::BeforeSimStep(event) = &runtime_event.payload {
+                    thread::sleep(Duration::from_millis(1));
+                    let partition = match runtime_event.meta.origin {
+                        EventOrigin::Partition(rank) => rank,
+                        EventOrigin::Controller => 0,
+                    };
+                    let _ = sender.send(BeforeSimStepMessage {
+                        partition,
                         iteration: runtime_event.meta.iteration,
                         seq_no: runtime_event.meta.seq_no,
-                        last_iteration: event.last_iteration,
+                        time: event.time,
                     });
                 }
             });
