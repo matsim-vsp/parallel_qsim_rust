88e028ae0138d4570eb495c7e7856391180bd270
apabis <aleksanderpabis@gmail.com>
2026-04-08 13:40:43 +0200

    Added single thread test for scoring

--- rust_qsim/tests/test_scoring.rs
+++ rust_qsim/tests/test_scoring.rs
@@ -6,7 +6,20 @@ use macros::integration_test;
 use rust_qsim::simulation::config::{CommandLineArgs, Config};
 
 #[integration_test(rust_qsim)]
-fn test_scoring() {
+fn test_scoring_single_part() {
+    let config_args = CommandLineArgs::new_with_path("./tests/resources/equil/equil-config-1-scoring.yml");
+    let config = Arc::new(Config::from_args(config_args));
+
+    TestExecutorBuilder::default()
+        .config(config)
+        .expected_events(None)
+        .build()
+        .unwrap()
+        .execute();
+}
+
+#[integration_test(rust_qsim)]
+fn test_scoring_2_parts() {
     let config_args = CommandLineArgs::new_with_path("./tests/resources/equil/equil-config-2-scoring.yml");
     let config = Arc::new(Config::from_args(config_args));
 
