3d462c7aea62db39c6f4a38eb9aab66504b29cf8
apabis <aleksanderpabis@gmail.com>
2026-04-09 13:52:23 +0200

    Revert "Added callback signature to the trait"
    This reverts commit bc43b9651fc8588eabb4da8422df3e8b6c7327df.

--- rust_qsim/src/simulation/messaging/sim_communication/mod.rs
+++ rust_qsim/src/simulation/messaging/sim_communication/mod.rs
@@ -26,10 +26,4 @@ pub trait SimCommunicator {
     fn barrier(&self);
 
     fn rank(&self) -> u32;
-
-    fn register_send_callback(&self, callback: Box<dyn Fn(&HashMap<u32, InternalSyncMessage>)>);
-
-    fn register_recv_callback(&self, callback: Box<dyn Fn(&InternalSyncMessage)>);
-    
-    fn register_comm_callback(&self, callback: Box<dyn Fn()>);
 }
