a1f88afb7ee6a09153f2ed0badc51cbb41d7ef5b
apabis <aleksanderpabis@gmail.com>
2026-04-30 14:40:57 +0200

    Updated Message trait

--- rust_qsim/src/simulation/scoring/mod.rs
+++ rust_qsim/src/simulation/scoring/mod.rs
@@ -1,12 +1,12 @@
+use std::any::{Any, TypeId};
 use crate::simulation::messaging::sim_communication::SimCommunicator;
 
 pub mod backpacking;
 
 /// A scoring engine contains a DataCollector and MessageBroker for respective implementation.
-pub trait ScoringEngine<C>
-where
-    C: SimCommunicator,
+pub trait ScoringEngine
 {
+    fn create_for_n_partitions(n: u32);
     fn scoring(&self);
 }
 
@@ -15,12 +15,24 @@ where
 //     fn register_fn(data_collector: Arc<Mutex<dyn DataCollector>>) -> Box<EventHandlerRegisterFn>;
 // }
 
-pub trait Message {
-    fn get_message(&self) -> &dyn Message;
+pub trait Message: Any + Send {
+    fn as_any(&self) -> &dyn Any;
+
+    fn into_any(self: Box<Self>) -> Box<dyn Any>;
+}
+
+impl<T: Any + Send> Message for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn into_any(self: Box<Self>) -> Box<dyn Any> {
+        self
+    }
 }
 
 pub struct InternalScoringMessage {
-    time: u32,
+    // time: u32,
     from_process: u32,
     to_process: u32,
     message: Box<dyn Message>
