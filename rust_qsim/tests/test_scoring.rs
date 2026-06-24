0feb4c497f59833a086d09730763d31403d0e859
apabis <aleksanderpabis@gmail.com>
2026-06-25 01:47:54 +0200

    Added mapping to test_scoring.rs

--- rust_qsim/tests/test_scoring.rs
+++ rust_qsim/tests/test_scoring.rs
@@ -4,7 +4,7 @@ use crate::test_simulation::TestExecutorBuilder;
 use std::sync::{Arc};
 use macros::integration_test;
 use rust_qsim::simulation::config::{CommandLineArgs, Config};
-use rust_qsim::simulation::config::ScoringPlansCollectionType::HomeSending;
+use rust_qsim::simulation::config::ScoringPlansCollectionType::{HomeSending, Mapping};
 
 #[integration_test(rust_qsim)]
 fn test_scoring_single_part_backpacking() {
@@ -34,6 +34,20 @@ fn test_scoring_single_part_homesending() {
 }
 
 #[integration_test(rust_qsim)]
+fn test_scoring_single_part_mapping() {
+    let config_args = CommandLineArgs::new_with_path("./tests/resources/equil/equil-config-1-scoring.yml");
+    let mut config = Config::from_args(config_args);
+    config.scoring_mut().plans_collection_type = Mapping;
+
+    TestExecutorBuilder::default()
+        .config(Arc::new(config))
+        .expected_events(None)
+        .build()
+        .unwrap()
+        .execute();
+}
+
+#[integration_test(rust_qsim)]
 fn test_scoring_2_parts_backpacking() {
     let config_args = CommandLineArgs::new_with_path("./tests/resources/equil/equil-config-2-scoring.yml");
     let config = Arc::new(Config::from_args(config_args));
@@ -51,6 +65,21 @@ fn test_scoring_2_parts_homesending() {
     let config_args = CommandLineArgs::new_with_path("./tests/resources/equil/equil-config-2-scoring.yml");
     let mut config = Config::from_args(config_args);
     config.scoring_mut().plans_collection_type = HomeSending;
+
+    TestExecutorBuilder::default()
+        .config(Arc::new(config))
+        .expected_events(None)
+        .build()
+        .unwrap()
+        .execute();
+}
+
+#[integration_test(rust_qsim)]
+fn test_scoring_2_parts_mapping() {
+    let config_args = CommandLineArgs::new_with_path("./tests/resources/equil/equil-config-2-scoring.yml");
+    let mut config = Config::from_args(config_args);
+    config.scoring_mut().plans_collection_type = Mapping;
+    config.scoring_mut().collector_threads = 2;
 
     TestExecutorBuilder::default()
         .config(Arc::new(config))
