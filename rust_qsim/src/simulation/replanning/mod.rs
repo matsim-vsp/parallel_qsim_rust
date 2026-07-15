3d1805c40de5312cb96cd3b365262ed14adbedbf
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-07-15 15:22:44 +0200

    fix syntax bug

--- rust_qsim/src/simulation/replanning/mod.rs
+++ rust_qsim/src/simulation/replanning/mod.rs
@@ -276,6 +276,8 @@ fn default_plan_remover() -> Box<dyn PlanSelector> {
     Box::new(WorstScoreSelector)
 }
 
+fn default_strategies() -> HashMap<Id<String>, Box<dyn PlanStrategy>> {
+    let mut strategies = HashMap::default();
     for selector in [
         DefaultSelector::KeepLastSelected,
         DefaultSelector::BestScore,
